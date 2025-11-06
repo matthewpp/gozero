65dbbdd23a23f64c0fb1fb1ecee89b2cc1ab957b
Pakapol <pakapol_p@muangthai.co.th>
2025-11-07 02:04:36 +0700

    re name

--- 13_context/main.go
+++ 13_context/main.go
@@ -14,11 +14,11 @@ import (
 func main() {
 
 	/* timeout context */
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 
 	select {
-	case <-time.After(3 * time.Second):
+	case <-time.After(10 * time.Second):
 		fmt.Println("operation completed.")
 	case <-ctx.Done():
 		fmt.Println("Operation time out:", ctx.Err())
